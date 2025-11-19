b53bdfe48d91b0a6ae4eec8adb43c595923a7bc4
Caio Amaral <147007596+caioamaral-io@users.noreply.github.com>
2025-11-19 14:37:13 -0300

    Dashboard v1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,20 +72,20 @@ const Navbar = () => {
 
 					<DropdownMenu>
 						<DropdownMenuTrigger asChild>
-							<Button variant="outline" size="icon">
-								<Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-								<Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+							<Button variant="outline" size="icon" className="h-8 w-8">
+								<Sun className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+								<Moon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
 								<span className="sr-only">Toggle theme</span>
 							</Button>
 						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end">
-							<DropdownMenuItem onClick={() => setTheme("light")}>
+						<DropdownMenuContent align="end" className="py-1 px-1 text-xs min-w-[6rem]">
+							<DropdownMenuItem className="py-2 text-xs" onClick={() => setTheme("light")}>
 								Light
 							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme("dark")}>
+							<DropdownMenuItem className="py-2 text-xs" onClick={() => setTheme("dark")}>
 								Dark
 							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme("system")}>
+							<DropdownMenuItem className="py-2 text-xs" onClick={() => setTheme("system")}>
 								System
 							</DropdownMenuItem>
 						</DropdownMenuContent>
